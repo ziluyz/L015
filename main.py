c6738f23cd166745512175c508b6dcfc67abebd5
Ilya <ziluyz@gmail.com>
2024-05-04 16:33:58 +0300

    add all consts and target img

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,6 +9,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("Img/icon.png")
 pygame.display.set_icon(icon)
+target_img = pygame.image.load("Img/pig.png")
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
